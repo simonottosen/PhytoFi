96203047329fc0519eb55b24d3196894596cb9a1
Casper Nørsø Pedersen <caspernorso@gmail.com>
2017-11-09 13:51:22 +0100

    TextInput til api

--- components/HomePage.js
+++ components/HomePage.js
@@ -5,12 +5,16 @@ export default class Movies extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
-    }
+      isLoading: true,
+      text: 'London'
+    };
   }
 
+  
+
   componentDidMount() {
-    return fetch('http://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=f873241aae3dee39adf62042e70a44c9')
+    
+    return fetch('http://api.openweathermap.org/data/2.5/weather?q='+this.state.text+'&APPID=f873241aae3dee39adf62042e70a44c9')
       .then((response) => response.json())
       .then((responseJson) => {
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -24,12 +28,14 @@ export default class Movies extends Component {
       .catch((error) => {
         console.error(error);
       });
+    
   }
 
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
+    
 
     if (this.state.isLoading) {
       return (
@@ -46,27 +52,30 @@ export default class Movies extends Component {
       <View style={{paddingTop: 50}}>
         <TextInput
           style={{height: 40}}
-          placeholder="Hej Rune! "
+          placeholder= {this.state.text}
           onChangeText={(text) => this.setState({text})}
+          
         />
 
-
+        
         <ListView
           dataSource={this.state.dataSource}
           renderRow={(rowData) => <Text>The weather today is going to be {rowData.main}.</Text>}
+      
         />
         <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text>{this.state.text}</Text>
         
         
+       
 
       </View>
 
-      
-
+    
 
 
     );
-
+   
     
   }
 }
