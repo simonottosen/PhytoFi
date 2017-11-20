15ac04a5ff7d9ca79f5e860b712b1e072c737ffc
Simon <simnik123@gmail.com>
2017-11-20 14:07:05 +0100

    Revert "Logo added to frontpage"
    This reverts commit 5c18d355324a621c0b39cd72f564467b8e6f352f.

--- components/Authentication.js
+++ components/Authentication.js
@@ -19,22 +19,19 @@ const {
 class Authentication extends Component {
   constructor() {
     super();
-    this.state = { username: '', password: '', city: '', uid: '', loading: false, error: '' };
-    this.profRef = firebase.database().ref().child('profile');
+    this.state = { username: '', password: '', loading: false, error: '' };
   }
-  
   userAuth() {
-    var that = this; 
     this.setState({ error: '', loading: true });
-    const { username, password, city } = this.state;
+    const { username, password } = this.state;
     firebase.auth().signInWithEmailAndPassword(username, password)
     .then(() => {
       this.setState({ error: '', loading: false });
-      var currentUser = firebase.auth().currentUser;
-      currentUser.getIdToken().then(function(idToken) {
-        that.setState({uid: currentUser.uid});
+      firebase.auth().currentUser.getIdToken().then(function(idToken) {
         AsyncStorage.setItem('id_token', idToken);
-        that.updateProfile(that);
+        console.log(idToken);
+        Alert.alert( 'Sign In Successfully!', 'Click the button to go to Home Page!');
+        Actions.HomePage();
       })
       .catch((err) => {
         this.setState({ error: 'Failed to obtain user ID token.'+err, loading: false });
@@ -43,56 +40,22 @@ class Authentication extends Component {
     .catch((err) => {
         //Login was not successful, let's create a new account
         firebase.auth().createUserWithEmailAndPassword(username, password)
-        .then(() => {
-          var currentUser = firebase.auth().currentUser;
+        .then(() => { 
           this.setState({ error: '', loading: false });
           firebase.auth().currentUser.getIdToken().then(function(idToken) {
-            that.setState({uid: currentUser.uid});
             AsyncStorage.setItem('id_token', idToken);
-            that.updateProfile(that);
+            console.log(idToken);
+            Alert.alert( 'Sign Up Successfully!', 'Click the button to go to Home Page!');
+            Actions.HomePage();
           })
           .catch(() => {
-            this.setState({ error: 'Failed to obtain user ID token.'+err, loading: false });
+            this.setState({ error: 'Failed to obtain user ID token.', loading: false });
           });
         })
         .catch((err) => {
             this.setState({ error: 'Authentication failed. '+err, loading: false });
         });
-        
     });
-  }
-  updateProfile(that) {
-    that.setState({ loading: true });
-    var data = {
-      city: that.state.city
-    };
-    that.profRef.child(that.state.uid).once('value', function(snapshot) {
-      if(snapshot.val() === null){
-        if(that.state.city == ''){
-          data.city == 'Copenhagen';
-        }
-        that.profRef.child(that.state.uid).set(data);
-        that.setState({ loading: false });
-      }else{
-        if(that.state.city != ''){
-          that.profRef.child(that.state.uid).update(data)
-          .then(() => {
-            that.setState({ loading: false });
-          })
-          .catch((err) => {
-            console.log(err);
-            that.setState({
-              loading: false
-            });
-          });
-        }else{
-          data.city = snapshot.val().city;
-        }
-      }
-      Alert.alert( 'Sign In Successfully!', 'Click the button to go to Home Page!');
-      Actions.HomePage({city: data.city});
-    })
-    .catch(err => console.error(err));
   }
   renderButtonOrSpinner() {
     if (this.state.loading) {
@@ -101,6 +64,7 @@ class Authentication extends Component {
     return <Button onPress={this.userAuth.bind(this)} title="Log in/Sign up" />;
   }
   render() {
+<<<<<<< HEAD
   
     return (
       <KeyboardAvoidingView style={styles.container} behavior='padding'>
@@ -110,6 +74,11 @@ class Authentication extends Component {
             width: 286, height: 160}}
             source={require('./LogoTrans.png')}
           />
+=======
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
+        <Text style={styles.title}>Welcome</Text>
+>>>>>>> parent of 06df4e7... Albert Magic 3
 
         <View style={styles.form}>
           <TitledInput
@@ -126,14 +95,6 @@ class Authentication extends Component {
             secureTextEntry
             value={this.state.password}
           />
-
-          <TitledInput
-            label='City'
-            onChangeText={(city) => this.setState({city})}
-            placeholder='City'
-            value={this.state.city}
-          />
-
           <Text style={styles.errorTextStyle}>{this.state.error}</Text>
           {this.renderButtonOrSpinner()}
         </View>
